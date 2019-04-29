8ab757ebac271b90079ef83af558f465c331054f
Kaisinel <almantusk@gmail.com>
2019-04-30 00:51:13 +0300

    Selenium Tutorial.

---
+++ SeleniumTests.Tests/SeleniumTests.Tests/SeleniumDriverFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests.Tests
+{
+    public class SeleniumDriverFixture:IDisposable
+    {
+        public IWebDriver Driver { get; }
+        const string homeUrl = "http://localhost:52701";
+        const string aboutPageUrl = "http://localhost:52701/Home/Contact";
+
+        public SeleniumDriverFixture()
+        {
+            var dir = Directory.GetCurrentDirectory();
+
+            Driver = new ChromeDriver(dir) { Url = homeUrl };
+        }
+
+        public void Dispose()
+        {
+            Driver.Dispose();
+        }
+
+    }
+}
